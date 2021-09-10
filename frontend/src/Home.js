35ff98009f4638f366542a6f55b6f5711c834e20
Beomsun <beomsun22@gmail.com>
2021-09-10 23:12:20 +0900

    버튼 추가

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 const Home = () => {
 
     const token = window.sessionStorage.getItem("token")
+
     function f1(){
     
         alert("클릭")
@@ -25,6 +26,26 @@ const Home = () => {
         })
     
     }
+    function f2(){
+    
+        alert("클릭")
+        axios({
+            method: "GET",
+            url: "http://localhost:8080/api/v2/member",
+            headers : {
+                "Auth" : token
+            }
+        }).then((response)=>{
+            console.log(response);
+            let data = JSON.stringify(response)
+            console.log(response.data("data"))
+            console.log(data)
+            
+        }).catch(Error=>{
+            console.log(Error);
+        })
+    
+    }
     
     return (
         <div>
@@ -32,7 +53,8 @@ const Home = () => {
             Home
             </h2>
             {token == null ? <a  href="http://localhost:8080/oauth2/authorization/google">구글로그인</a> : "login sucess"}
-            {token == null ? null : <button onClick={() => { f1();}}>Click Me</button>}
+            {token == null ? null : <button onClick={() => { f1();}}>Click Me1</button>}
+            {token == null ? null : <button onClick={() => { f2();}}>Click Me2</button>}
         </div>
         
     );
