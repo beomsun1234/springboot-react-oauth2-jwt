af74c6f1bd1734ed7aae5b77c63e5b4982adf033
Beomsun <beomsun22@gmail.com>
2021-09-10 22:31:09 +0900

    ADD - CROS오리진 설정, 프른트와 연동성공

--- backend/src/main/java/com/bs/hellooauth2jwt/controller/Controller.java
+++ backend/src/main/java/com/bs/hellooauth2jwt/controller/Controller.java
@@ -1,10 +1,13 @@
 
 package com.bs.hellooauth2jwt.controller;
 
+import com.bs.hellooauth2jwt.domain.Member;
+import com.bs.hellooauth2jwt.oauth.CustomUserDetail;
 import io.jsonwebtoken.Jwts;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +26,13 @@ public class Controller {
     public String member(){
         log.info("인증된 사용자가 api/member요청");
         return "환영합니다";
+    }
+
+    //
+
+    @GetMapping("/api/v2/member")
+    public Member memberV2(@AuthenticationPrincipal OAuth2User oAuth2User){
+        CustomUserDetail customUserDetail = (CustomUserDetail)oAuth2User;
+        return customUserDetail.getMember();
     }
 }
