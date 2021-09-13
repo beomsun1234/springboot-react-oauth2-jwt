5e85ff4acf1e4a09f2f6b637cd1a01e28bcef9b4
Beomsun <beomsun22@gmail.com>
2021-09-13 22:48:58 +0900

    ADD - 프론트에서 oauth2로그인 진행 후 완료하면 jwt토큰 발급
    next - 구글, 카카오, 페이스북을 나누어 jwt토큰 발급

--- backend/src/main/java/com/bs/hellooauth2jwt/controller/AuthController.java
+++ backend/src/main/java/com/bs/hellooauth2jwt/controller/AuthController.java
@@ -3,11 +3,13 @@ package com.bs.hellooauth2jwt.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 @Slf4j
 @RequestMapping("auth")
@@ -18,5 +20,11 @@ public class AuthController {
         log.info("로그인 성공");
         log.info("header={}", request.getHeader("Authorization"));
         return token;
+    }
+
+    @PostMapping("token")
+    public String  token(Map<String, Object> attributes){
+        log.info("attributes={}", attributes);
+        return "token";
     }
 }
