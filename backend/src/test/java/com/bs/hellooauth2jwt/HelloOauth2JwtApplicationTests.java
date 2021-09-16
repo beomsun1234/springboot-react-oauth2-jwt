140e01e4d582bc0d79f99a54de5a5915fd713bf0
Beomsun <beomsun22@gmail.com>
2021-09-17 00:25:12 +0900

    EDIT - test

--- backend/src/test/java/com/bs/hellooauth2jwt/HelloOauth2JwtApplicationTests.java
+++ backend/src/test/java/com/bs/hellooauth2jwt/HelloOauth2JwtApplicationTests.java
@@ -3,11 +3,11 @@ package com.bs.hellooauth2jwt;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class HelloOauth2JwtApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class HelloOauth2JwtApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
